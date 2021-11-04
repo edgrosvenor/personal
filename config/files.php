c6ca943c3f826882ecad38490925825b2507c204
Ed Grosvenor <ed.grosvenor@wickedviral.com>
2021-11-04 18:17:56 -0500

    Fix path problem

--- config/files.php
+++ config/files.php
@@ -3,13 +3,13 @@
 return [
     'README.md' => [
         'name' => 'README.md',
-        'url' => 'README.md',
+        'url' => '/README.md',
         'tooltip' => 'Go back to square one and start again.',
     ],
     '.editorconfig' => [
         'icon' => 'fa fa-gear',
         'name' => '.editorconfig',
-        'url' => 'resources/md/editor.md',
+        'url' => '/resources/md/editor.md',
         'tooltip' => 'Read up on how I configure my PHPStorm',
     ],
 ];
